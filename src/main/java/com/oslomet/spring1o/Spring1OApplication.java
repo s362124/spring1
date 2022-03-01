454e0f309bf16c847bb738e64aadda3249de8585
s362124 <s362124@oslomet.no>
2022-03-01 15:00:17 +0100

    Initial commit

---
+++ src/main/java/com/oslomet/spring1o/Spring1OApplication.java
@@ -0,0 +1,13 @@
+package com.oslomet.spring1o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1OApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring1OApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oslomet/spring1o/Spring1OApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oslomet.spring1o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1OApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
